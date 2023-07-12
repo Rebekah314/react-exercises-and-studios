1e21f9dc3612763d1d4cb9cef8e20be85d381516
Rebekah Garris <rebekahlgarris@gmail.com>
2023-07-12 12:44:12 -0700

    finished with Part 1

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,28 @@
+import classes from './BookList.module.css';
+
+
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books I Want to Read";
+   let book1 = "http://embed.cdn.pais.scholastic.com/v1/products/identifiers/isbn/9780439358064/primary/renditions/600?useMissingImage=true";
+   let book2 = "http://embed.cdn.pais.scholastic.com/v1/products/identifiers/isbn/9780439784542/primary/renditions/600?useMissingImage=true";
+   let book3 = "http://embed.cdn.pais.scholastic.com/v1/products/identifiers/isbn/9780545010221/primary/renditions/600?useMissingImage=true";
 
    return (
       <div>
-         <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <h3 className = {classes.bookHeading}>{pageTitle}</h3>
+         <table>
+            <tr>
+               <td>
+               <img src={book1} alt="Harry Potter Book 5 cover image" />
+               </td>
+               <td>
+               <img src={book2} alt="Harry Potter Book 6 cover image" />
+               </td>
+               <td>
+               <img src={book3} alt="Harry Potter Book 7 cover image" />
+               </td>
+            </tr></table>
+
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.js
+++ part1/exercises/src/components/ChoresList.js
@@ -1,3 +1,18 @@
+import classes from './ChoresList.module.css';
+
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   return( 
+   <div>
+      <h3 className = {classes.choresHeading}>
+         Chores I Love to Do
+      </h3>
+      <p className = {classes.choresText}>
+         <ol>
+            <li>Run the dishwasher</li>
+            <li>Feed the kids</li>
+            <li>Walk the dog</li>
+         </ol>
+      </p>;
+   </div>
+   );
 }
