b6876144ca3a1ca9224670ecdebf939348a2b34a
Rebekah Garris <rebekahlgarris@gmail.com>
2023-07-12 13:03:07 -0700

    finished part 2

---
+++ part1/exercises/src/components/HobbyLinks.js
@@ -0,0 +1,15 @@
+import classes from './HobbyLinks.module.css';
+
+export default function HobbyLinks() {
+    const hobbyLinks = ["https://organize365.com/", "https://drmindypelz.com/"];
+
+    return(
+        <div>
+            <h3>Links for My Hobbies</h3>
+            <ul>
+                <li><label>Personal Organization: <a href = {hobbyLinks[0]}>Organize 365</a></label></li>
+                <li><label>Fasting for Women: <a href = {hobbyLinks[1]}>Dr. Mindy Pelz</a></label></li>
+            </ul>
+        </div>
+    )
+}
