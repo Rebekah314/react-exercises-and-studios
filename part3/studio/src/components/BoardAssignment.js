487b7435c9864575f62c460f0c51b3fa327dce84
Rebekah Garris <rebekahlgarris@gmail.com>
2023-08-02 12:25:18 -0500

    Updating files for Part3 studio

--- part3/studio/src/components/BoardAssignment.js
+++ part3/studio/src/components/BoardAssignment.js
@@ -1,18 +1,36 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-export default function BoardAssignment () {
-   const boards = [];
+export default function BoardAssignment() {
+  const boards = [
+    {
+      label: "Vegetarian Recipes",
+      value: 0,
+    },
+    {
+      label: "Healthy Meals",
+      value: 1,
+    },
+    {
+      label: "Soups",
+      value: 2,
+    },
+  ];
+  const [boardName, setName] = useState("no boards yet!");
 
-   const handleChange = (event) => {
-   }
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
 
-   return (
-      <div style={{paddingTop: "50px"}}>
+  return (
+    <div style={{ paddingTop: "50px" }}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+        {boards.map((item) => {
+          return <option value={item.label}>{item.label}</option>;
+         })}
       </select>
 
       <p>Saved to {boardName}!</p>
-      </div>
-   );
+    </div>
+  );
 }
--- part3/studio/src/components/Recipe.js
+++ part3/studio/src/components/Recipe.js
@@ -1,11 +1,11 @@
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   let authorLink = "https://theschmidtywife.com/simple-vegetable-rice-soup/";
+   let authorPhoto = "https://theschmidtywife.com/wp-content/uploads/2022/05/homepage-about.jpg";
+   let authorName = "Lauren, the Schmidty Wife";
 
    return (
       <div>
-         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
+         <img src={authorPhoto} alt ={authorName} style={{objectFit: "contain", borderRadius: "50%"}} />
          <div>
             <h3>{authorName}</h3>
             <a href={authorLink}></a> 
@@ -15,7 +15,7 @@ const RecipeAuthor = () => {
 }
 
 const RecipeIngredients = () => {
-   const ingredients = [];
+   const ingredients = ["broth", "onions", "carrots", "celery", "olive oil"];
    return(
       <div>
          <h3>Recipe Ingredients</h3>
@@ -34,8 +34,10 @@ const RecipeDescription = () => {
    return (
       <div> 
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>Simple Vegetable Rice Soup</h1>
+            <p>Perfect for a light meal or a side dish this simple vegetable rice soup 
+               is made with classic vegetables, chicken broth, and rice. Reminiscent 
+               of chicken noodle soup but simpler and lighter.</p>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
@@ -47,7 +49,8 @@ const RecipeDescription = () => {
 
 const RecipePhoto = () => {
    return (
-      <img src="" alt="" className="imageUpdates"/>
+      <img src="https://theschmidtywife.com/wp-content/uploads/2019/02/pot-of-simple-vegetable-rice-soup-480x480.jpg" 
+      alt="Simple Vegetable Rice Soup" className="imageUpdates"/>
    );
 }
 
